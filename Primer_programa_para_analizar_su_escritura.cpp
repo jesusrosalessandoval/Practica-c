d3ed23ed3002a09a78ec10d913ec7577267a841e
jesusrosalessandoval <155786326+jesusrosalessandoval@users.noreply.github.com>
2024-01-11 19:17:26 -0400

    Add files via upload

---
+++ Primer_programa_para_analizar_su_escritura.cpp
@@ -0,0 +1,16 @@
+/*
+Titulo: Primer programa con comentario
+Autor: Juan Jesus Rosales Sandoval
+Descripcion: Primer programa par analizar su escritura
+Fecha: 2024-01-10
+*/
+#include "iostream"//Llama la libreria namespace
+	using namespace std;//Dar acseso al espacio std
+int main()//Funcion principal para retornar un entero
+{
+	int numero;//Declaracion de la variable numero como entero
+	
+	numero = 5 + 5;//Asignacion de la suma de 5+5 a la variable numero
+	cout<<"El resultado de la suma es: "<<numero; "\n";//Imprescion de la variable numero y al final hasemos un salto de linea
+	return 0;//Retorno de 0 para finalizar el programa
+}
