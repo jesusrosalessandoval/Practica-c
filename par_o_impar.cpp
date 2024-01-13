f2cce4af77554ecc8a601ffbc37961af0f32be42
jesusrosalessandoval <155786326+jesusrosalessandoval@users.noreply.github.com>
2024-01-12 21:51:21 -0400

    Add files via upload

---
+++ par_o_impar.cpp
@@ -0,0 +1,25 @@
+/*
+TITULO:PAR o IMPAR
+AUTOR:Juan Jesus Rosales Sandoval
+DESCRIPCION: Solicitar un numero  y verificar si es par o impar
+FECHA:12/01/2024
+*/
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+	int numero;
+	cout << "ingrese el numero ";
+	cin >> numero;
+	if(numero % 2)
+	{
+		cout << "El nuemro es Impar\n";
+	}
+	else 
+	{
+	   cout<<"El numero es Par! \n";
+	}
+	return 0;
+}
