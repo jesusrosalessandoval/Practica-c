ec1eed9126f0f77d2edd1617f922d5149590a398
jesusrosalessandoval <155786326+jesusrosalessandoval@users.noreply.github.com>
2024-01-12 21:40:35 -0400

    Add files via upload

---
+++ uso_cin_y_cout.cpp
@@ -0,0 +1,22 @@
+/*
+TITULO:USO DE STRING
+AUTOR:Juan Jesus Rosales Sandoval
+DESCRIPCION: Declaracion de uso de cin
+FECHA:12/01/2024
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+	string cadena1,cadena2,universidad;
+	cadena1="Bienvenido a ";
+	cadena2="universidad ";
+	cout<< "Ingrese el nombre de la universidad: ";
+	cin>>universidad;
+	cout<<cadena1<<"la "<<cadena2<<" "<<universidad<<endl;
+	return 0;
+}
+
