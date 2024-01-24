bcf661048a698b8ec9aaa65a8212e54a443814fa
jesusrosalessandoval <155786326+jesusrosalessandoval@users.noreply.github.com>
2024-01-23 20:19:49 -0400

    Add files via upload

---
+++ Funcion_sumar.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <random>
+#include <limits>
+
+using namespace std;
+int n1,n2;
+//declaracion de functiones
+int sumarNumeros()
+{
+	int sumarNumeros;		
+	sumarNumeros=n1+n2;	
+	return sumarNumeros;
+}
+void Numeros()
+{
+	cout<<"Ingrese el primer numero: ";
+	cin>>n1;
+	cout<<"Ingrese el segundo numero: ";
+	cin>>n2;
+}
+int main(int argc, char *argv[]) {
+	int n1,n2;
+	Numeros();
+	cout<<"Suma de 2 numero: " <<sumarNumeros()<<endl;
+	Numeros();
+	cout<<"Suma de 2 numero: " <<sumarNumeros()<<endl;
+	return 0;
+	}
