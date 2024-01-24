73709dc99097ed22db7543897adacd91ba3b2fbb
jesusrosalessandoval <155786326+jesusrosalessandoval@users.noreply.github.com>
2024-01-23 20:47:48 -0400

    Funcion_sumar.cpp

--- Funcion_sumar.cpp
+++ Funcion_sumar.cpp
@@ -8,21 +8,21 @@ int n1,n2;
 int sumarNumeros()
 {
 	int sumarNumeros;		
-	sumarNumeros=n1+n2;	
+	sumarNumeros=n1+n2;	//Suma nuestra variable
 	return sumarNumeros;
 }
-void Numeros()
+void Numeros()//Tipo de valor vacio
 {
 	cout<<"Ingrese el primer numero: ";
-	cin>>n1;
+	cin>>n1;//Ingreso de variavles
 	cout<<"Ingrese el segundo numero: ";
-	cin>>n2;
+	cin>>n2;//Ingreso de variavles
 }
 int main(int argc, char *argv[]) {
 	int n1,n2;
 	Numeros();
-	cout<<"Suma de 2 numero: " <<sumarNumeros()<<endl;
+	cout<<"Suma de 2 numero: " <<sumarNumeros()<<endl;//Nuestra la suma total de las variables
 	Numeros();
-	cout<<"Suma de 2 numero: " <<sumarNumeros()<<endl;
+	cout<<"Suma de 2 numero: " <<sumarNumeros()<<endl;//Nuestra la suma total de las variables
 	return 0;
 	}
