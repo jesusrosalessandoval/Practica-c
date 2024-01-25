0f6ebe9afba5e52b64c3b202e5f59229afb4e48b
jesusrosalessandoval <155786326+jesusrosalessandoval@users.noreply.github.com>
2024-01-24 21:53:10 -0400

    Add files via upload

---
+++ Num_Aleatorio_con_funciones.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int Aleatorio() {
+	return rand() % 100;  
+}
+bool esImpar(int numero){//generador de numeros aleatorios
+	return numero %2!=0;
+}
+int main(){
+	int n,c=0,num;
+	cout<<"Generar n numero Impares, ingrese n: ";//imprimir las veses que el usuario ponga
+	cin>>n;
+	while(c<n)
+	{
+		num=Aleatorio();
+		if(esImpar(num))
+		{
+			c++;
+			cout<<"impar "<<c<<" :"<<num<<endl;//mostrara las veses que el usuario solicite
+		}
+	}
+}
