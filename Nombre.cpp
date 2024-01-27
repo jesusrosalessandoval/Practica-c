aa6f394422d2e95040871f6dd29ffd7aa414cf00
jesusrosalessandoval <155786326+jesusrosalessandoval@users.noreply.github.com>
2024-01-26 21:54:18 -0400

    Add files via upload

---
+++ Nombre.cpp
@@ -0,0 +1,21 @@
+/*
+Autor:juan jesus rosales sandoval
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+string NombreCompleto(const string& nombre1, const string& nombre2, const string& apepat, const string& apemat)
+{
+	return apepat + " " + apemat + " " + nombre1 + " " + nombre2;
+}
+int main()
+{
+	string nombre1, nombre2, apepat, apemat;
+	cout << "Ingresa el nombre completo: ";
+	cin >> nombre1 >> nombre2 >> apepat >> apemat;
+	string nombreCompleto = NombreCompleto(nombre1, nombre2, apepat, apemat);
+	cout << "Nombre completo: " << nombreCompleto << endl;
+	return 0;
+} 
