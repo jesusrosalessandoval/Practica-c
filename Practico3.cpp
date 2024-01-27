3e7b71b573b4e6828633fc97f7f129017640d80d
jesusrosalessandoval <155786326+jesusrosalessandoval@users.noreply.github.com>
2024-01-26 21:54:30 -0400

    Add files via upload

---
+++ Practico3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void menuPrincipal() {
+
+	cout<<"Menu Principal\n";
+	cout<<"(1)-String suma de una cadena\n";
+	cout<<"(2)-String Nombre Completo\n";
+	cout<<"(3)-String ofuscar\n";
+	cout<<"(s/S)-Salir"
+}
+
+int main(int argc, char *argv[]) {
+	
+	char opcion;	
+	do
+		cout<<"Elija una opcion";
+		cin>>opcion;
+		switch (opcion) {
+		case '1':
+		{
+			
+		}	
+			break;
+		case '2':
+		{
+			
+
+		}
+		break;
+		case 's':
+			cout << "Saliendo del programa." << endl;
+			break;
+		default:
+			cout << "Opción no válida. Inténtalo de nuevo." << endl;
+			break;
+		}
+		
+	} while (opcion != 's' && opcion != 'S');
+
+	return 0;
+}
+
